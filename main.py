42feffd05196a41d98f16cabde436e4c18805df1
Teptphanyaki <ethan.wang.c@gmail.com>
2017-03-12 23:03:54 -0700

    main file set up

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import packet
 import host
 
 # SIFS = 0.05 msec and DIFS = 0.1 msec. Your CSMA/CA implementation can sense the channel every 0.01 msec.
+# number_of_host = int(input("Please enter number of hosts: "))
+number_of_host = 10
+
+host_list = list()
+
+for i in range(number_of_host):
+    host_list.append(host.host())
+
+for i in range(100000):  # 100000 msec
+    for host in host_list:
+        # do stuff
