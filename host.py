a5aae92ab24582b592887998ed7a2cb95903f671
Justin Jia <justin.jia.developer@gmail.com>
2017-03-14 14:39:22 -0700

    Add a basic run loop

--- host.py
+++ host.py
@@ -59,86 +59,94 @@ class host(object):
     def is_idle(self):
         return
 
-    def generate_frame_size():
-        u = random.random()
-        return ((-1 / 1544) * log(1 - u))
-
-    def generate_arrival_time():
-        u = random.random()
-        return ((-1 / arrival_rate) * log(1 - u))
-
-    def generate_service_time():
-        return 5  # SIFS = 0.05 msec
-
-    def generate_packet():
-        return packet.Packet(generate_service_time())
+    # def generate_frame_size():
+    #     u = random.random()
+    #     return ((-1 / 1544) * log(1 - u))
+    #
+    # def generate_arrival_time():
+    #     u = random.random()
+    #     return ((-1 / arrival_rate) * log(1 - u))
+    #
+    # def generate_service_time():
+    #     return 5  # SIFS = 0.05 msec
+    #
+    # def generate_packet():
+    #     return packet.Packet(generate_service_time())
 
 # configurations
 service_rate = float(input("Please enter the service rate: "))
 
-# statistics
-total_server_busy_time = 0
-total_active_packets_length = 0
-total_packet_queue_length = 0
-total_packets = 0
-total_active_packets = 0
-total_dropped_packets = 0
+hosts = []
+channel = []
 
-current_time = 0
-server_busy_start_time = -1
+for current_time in range(100000):
+    channel_is_idle = len(channel) == 0
+    for host in host:
+        host.timestep(channel_is_idle)
 
-# initialize
-packet_queue = queue.Queue(MAXBUFFER)
-event_list = GEL.GEL()
-
-# simulate
-event_list.schedule("arrival", generate_arrival_time(), generate_packet())
-
-for i in range(100000):
-    event = event_list.pop()
-    current_time = event.time
-    # print(event.time)
-    if event.type == "arrival":
-        event_list.schedule("arrival", current_time + generate_arrival_time(), generate_packet())
-        if total_active_packets == 0:
-            total_active_packets_length += total_active_packets
-            total_packet_queue_length += packet_queue.qsize()
-            total_packets += 1
-            event_list.schedule("departure", current_time + event.packet.service_time, event.packet)
-            total_active_packets += 1
-            if server_busy_start_time == -1:
-                server_busy_start_time = current_time
-        elif (total_active_packets < MAXBUFFER + 1) or (MAXBUFFER == 0):
-            total_active_packets_length += total_active_packets
-            total_packet_queue_length += packet_queue.qsize()
-            total_packets += 1
-            packet_queue.put(event.packet)
-            total_active_packets += 1
-        else:
-            total_dropped_packets += 1
-    elif event.type == "departure":
-        total_active_packets -= 1
-        if total_active_packets == 0:
-            if server_busy_start_time != -1:
-                total_server_busy_time += current_time - server_busy_start_time
-                server_busy_start_time = -1
-        if total_active_packets > 0:
-            next_packet = packet_queue.get()
-            event_list.schedule("departure", current_time + next_packet.service_time, next_packet)
-
-if server_busy_start_time != -1:
-    total_server_busy_time += current_time - server_busy_start_time
-
-# results
-print("--------------------------------------")
-print("Server utilization:", end=' ')
-print(total_server_busy_time / current_time)
-print("Average active packets length:", end=' ')
-print(total_active_packets_length / total_packets)
-print("Average packets queue length:", end=' ')
-print(total_packet_queue_length / total_packets)
-print("Packet drop rate:", end=' ')
-print(total_dropped_packets / total_packets)
-print("Total Packet dropped:", end=' ')
-print(total_dropped_packets)
-print("--------------------------------------")
+# # statistics
+# total_server_busy_time = 0
+# total_active_packets_length = 0
+# total_packet_queue_length = 0
+# total_packets = 0
+# total_active_packets = 0
+# total_dropped_packets = 0
+#
+# current_time = 0
+# server_busy_start_time = -1
+#
+# # initialize
+# packet_queue = queue.Queue(MAXBUFFER)
+# event_list = GEL.GEL()
+#
+# # simulate
+# event_list.schedule("arrival", generate_arrival_time(), generate_packet())
+#
+# for i in range(100000):
+#     event = event_list.pop()
+#     current_time = event.time
+#     # print(event.time)
+#     if event.type == "arrival":
+#         event_list.schedule("arrival", current_time + generate_arrival_time(), generate_packet())
+#         if total_active_packets == 0:
+#             total_active_packets_length += total_active_packets
+#             total_packet_queue_length += packet_queue.qsize()
+#             total_packets += 1
+#             event_list.schedule("departure", current_time + event.packet.service_time, event.packet)
+#             total_active_packets += 1
+#             if server_busy_start_time == -1:
+#                 server_busy_start_time = current_time
+#         elif (total_active_packets < MAXBUFFER + 1) or (MAXBUFFER == 0):
+#             total_active_packets_length += total_active_packets
+#             total_packet_queue_length += packet_queue.qsize()
+#             total_packets += 1
+#             packet_queue.put(event.packet)
+#             total_active_packets += 1
+#         else:
+#             total_dropped_packets += 1
+#     elif event.type == "departure":
+#         total_active_packets -= 1
+#         if total_active_packets == 0:
+#             if server_busy_start_time != -1:
+#                 total_server_busy_time += current_time - server_busy_start_time
+#                 server_busy_start_time = -1
+#         if total_active_packets > 0:
+#             next_packet = packet_queue.get()
+#             event_list.schedule("departure", current_time + next_packet.service_time, next_packet)
+#
+# if server_busy_start_time != -1:
+#     total_server_busy_time += current_time - server_busy_start_time
+#
+# # results
+# print("--------------------------------------")
+# print("Server utilization:", end=' ')
+# print(total_server_busy_time / current_time)
+# print("Average active packets length:", end=' ')
+# print(total_active_packets_length / total_packets)
+# print("Average packets queue length:", end=' ')
+# print(total_packet_queue_length / total_packets)
+# print("Packet drop rate:", end=' ')
+# print(total_dropped_packets / total_packets)
+# print("Total Packet dropped:", end=' ')
+# print(total_dropped_packets)
+# print("--------------------------------------")
