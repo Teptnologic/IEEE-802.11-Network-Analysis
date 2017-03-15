a653cb20da2217247d982196fb88ef69bfa58132
Justin Jia <justin.jia.developer@gmail.com>
2017-03-14 23:58:36 -0700

    Finish project 2

--- frame.py
+++ frame.py
@@ -1,5 +1,7 @@
 class Frame(object):
-    def __init__(self, process_time, source, destination, is_ack):
+    def __init__(self, bytes, scheduled_time, process_time, source, destination, is_ack):
+        self.bytes = bytes
+        self.scheduled_time = scheduled_time
         self.process_time = process_time
         self.source = source
         self.destination = destination
--- main.py
+++ main.py
@@ -5,52 +5,52 @@ import frame
 
 # constants
 # a host can sense the channel every 0.01 msec
-BASE_TIME = 100  # 0.01 ms * BASE_TIME = 1 ms
-ACK_PROCESS_TIME = 64 * 8 / (11 * (10 ** 6)) * (10 ** 3) * BASE_TIME
-DEFAULT_DIFS = 0.1 * BASE_TIME  # 0.1 ms
-DEFAULT_SIFS = 0.05 * BASE_TIME  # 0.05 ms
-
+MAX_FRAME_SIZE = 1544
+ACK_FRAME_SIZE = 64
 BITS_PER_BYTE = 8
 WIRELESS_CAPACITY = (11 * (10 ** 6))  # 11 Mbps
 SENSE_CHANNEL_INTERVAL = 100  # 0.01 ms
-
-# configurations
-# backoff_time_cap = float(input("Please enter the backoff time cap (ms): "))
-backoff_time_cap = 3
-# timeout = float(input("Please enter the timeout (ms): "))
-timeout = 3
-# arrival_rate = float(input("Please enter the arrival rate: "))
-arrival_rate = 0.9
-# number_of_host = int(input("Please enter number of hosts: "))
-number_of_host = 20
+BASE_TIME = 100  # 0.01 ms * BASE_TIME = 1 ms
+TRANSMISSION_SPEED = (11 * (10 ** 6)) / ((10 ** 3) * BASE_TIME)
+SIMULATION_TIME = 10 * (10 ** 3) * BASE_TIME  # 10 s
+ACK_PROCESS_TIME = ACK_FRAME_SIZE * BITS_PER_BYTE / TRANSMISSION_SPEED
+DEFAULT_DIFS = 0.1 * BASE_TIME  # 0.1 ms
+DEFAULT_SIFS = 0.05 * BASE_TIME  # 0.05 ms
 
 
 def generate_frame_size():
     u = random.random()
-    return ((-1 / 1544) * log(1 - u)) * BITS_PER_BYTE
+    return (-1 * log(1 - u)) * MAX_FRAME_SIZE
 
 
-def generate_arrival_time():
+def generate_arrival_time(arrival_rate):
     u = random.random()
     return ((-1 / arrival_rate) * log(1 - u)) * (10 ** 3) * BASE_TIME
 
 
-def generate_backoff_time():
+def generate_backoff_time(backoff_time_cap):
     u = random.random()
     return u * backoff_time_cap * BASE_TIME
 
 
-def generate_process_time():
-    return generate_frame_size() / WIRELESS_CAPACITY * (10 ** 3) * BASE_TIME
+def generate_process_time(bytes):
+    return bytes * BITS_PER_BYTE / WIRELESS_CAPACITY * (10 ** 3) * BASE_TIME
 
 
-def generate_destination():
+def generate_destination(number_of_host):
     u = random.random()
     return int(u * number_of_host)
 
 
+# configurations
+backoff_time_cap = float(input("Please enter the backoff time cap (ms): "))
+timeout = float(input("Please enter the timeout (ms): "))
+arrival_rate = float(input("Please enter the arrival rate: "))
+number_of_host = int(input("Please enter number of hosts: "))
+
 # statistics
-conflict_times = 0
+transmitted_bytes = 0
+total_delay = 0
 
 # initializations
 hosts = []
@@ -58,40 +58,44 @@ arrival_times = []
 channel = []
 for i in range(number_of_host):
     hosts.append(host.Host(DEFAULT_DIFS, DEFAULT_SIFS))
-    arrival_times.append(generate_arrival_time())
+    arrival_times.append(generate_arrival_time(arrival_rate))
 
 # main
-for current_time in range(100 * (10 ** 3) * BASE_TIME):
+for current_time in range(SIMULATION_TIME):
     channel_is_idle = len(channel) == 0
     channel_has_conflicts = len(channel) > 1
-    # update statistics
-    if channel_has_conflicts:
-        conflict_times += 1
     # schedule data frames
     for i in range(number_of_host):
         current_host = hosts[i]
         arrival_time = arrival_times[i]
         if arrival_time <= 0:
+            size = generate_frame_size()
             data_frame = frame.Frame(
-                generate_process_time(),
+                size,
+                current_time,
+                generate_process_time(size),
                 i,
-                generate_destination(),
+                generate_destination(number_of_host),
                 False
             )
             current_host.schedule(data_frame)
-            arrival_times[i] = generate_arrival_time()
+            arrival_times[i] = generate_arrival_time(arrival_rate)
         else:
             arrival_times[i] -= 1
     # update frames in channel
     for current_frame in channel:
         current_frame.process_time -= 1
         current_frame.is_dirty = channel_has_conflicts
-        if current_frame.process_time <= 0:  # received frame
+        if current_frame.process_time <= 0 and not current_frame.is_dirty:
             if current_frame.is_ack:  # received ack frame
                 hosts[current_frame.destination].timeout = -1  # complete timer
             else:  # received data frame
+                transmitted_bytes += current_frame.bytes
+                total_delay += current_time - current_frame.scheduled_time
                 ack_frame = frame.Frame(
-                    generate_process_time(),
+                    ACK_FRAME_SIZE,
+                    current_time,
+                    generate_process_time(ACK_FRAME_SIZE),
                     current_frame.destination,
                     current_frame.source,
                     True,
@@ -101,9 +105,10 @@ for current_time in range(100 * (10 ** 3) * BASE_TIME):
     channel[:] = [x for x in channel if x.process_time > 0]
     # send frames
     for current_host in hosts:
+        size = generate_frame_size()
         current_frame = current_host.sent_frame(
             channel_is_idle,
-            generate_backoff_time(),
+            generate_backoff_time(backoff_time_cap),
             timeout * BASE_TIME,
             DEFAULT_DIFS,
             DEFAULT_SIFS
@@ -112,4 +117,9 @@ for current_time in range(100 * (10 ** 3) * BASE_TIME):
             channel.append(current_frame)
 
 # results
-print(conflict_times)
+simulation_time_in_seconds = SIMULATION_TIME / (10 ** 3) / BASE_TIME
+total_delay_in_seconds = total_delay / (10 ** 3) / BASE_TIME
+throughput = transmitted_bytes / simulation_time_in_seconds
+average_delay = total_delay_in_seconds / throughput
+print("Throughtput: " + str(throughput) + " bytes/s")
+print("Average network delay: " + str(average_delay) + " s")
