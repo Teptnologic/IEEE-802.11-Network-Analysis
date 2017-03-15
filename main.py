522d027b954ec8b2108b6575147379ea040150ff
Justin Jia <justin.jia.developer@gmail.com>
2017-03-14 22:29:24 -0700

    Fix a few issues

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ BASE_TIME = 100  # 0.01 ms * BASE_TIME = 1 ms
 ACK_PROCESS_TIME = 64 * 8 / (11 * (10 ** 6)) * (10 ** 3) * BASE_TIME
 DEFAULT_DIFS = 0.1 * BASE_TIME  # 0.1 ms
 DEFAULT_SIFS = 0.05 * BASE_TIME  # 0.05 ms
-DEFAULT_BACKOFF = 1 * BASE_TIME  # TODO: use actual number
 
 BITS_PER_BYTE = 8
 WIRELESS_CAPACITY = (11 * (10 ** 6))  # 11 Mbps
@@ -17,11 +16,13 @@ SENSE_CHANNEL_INTERVAL = 100  # 0.01 ms
 
 # configurations
 # backoff_time_cap = float(input("Please enter the backoff time cap (ms): "))
-backoff_time_cap = 10
+backoff_time_cap = 3
+# timeout = float(input("Please enter the timeout (ms): "))
+timeout = 3
 # arrival_rate = float(input("Please enter the arrival rate: "))
-arrival_rate = 0.1
+arrival_rate = 0.9
 # number_of_host = int(input("Please enter number of hosts: "))
-number_of_host = 10
+number_of_host = 20
 
 
 def generate_frame_size():
@@ -60,7 +61,7 @@ for i in range(number_of_host):
     arrival_times.append(generate_arrival_time())
 
 # main
-for current_time in range(10 * (10 ** 3) * BASE_TIME):
+for current_time in range(100 * (10 ** 3) * BASE_TIME):
     channel_is_idle = len(channel) == 0
     channel_has_conflicts = len(channel) > 1
     # update statistics
@@ -85,25 +86,30 @@ for current_time in range(10 * (10 ** 3) * BASE_TIME):
     for current_frame in channel:
         current_frame.process_time -= 1
         current_frame.is_dirty = channel_has_conflicts
-        if current_frame.process_time <= 0:
-            ack_frame = frame.Frame(
-                generate_process_time(),
-                current_frame.destination,
-                current_frame.source,
-                True,
-            )
-            hosts[current_frame.destination].schedule(ack_frame)
+        if current_frame.process_time <= 0:  # received frame
+            if current_frame.is_ack:  # received ack frame
+                hosts[current_frame.destination].timeout = -1  # complete timer
+            else:  # received data frame
+                ack_frame = frame.Frame(
+                    generate_process_time(),
+                    current_frame.destination,
+                    current_frame.source,
+                    True,
+                )
+                hosts[current_frame.destination].schedule(ack_frame)
     # remove delivered frames in channel
     channel[:] = [x for x in channel if x.process_time > 0]
     # send frames
     for current_host in hosts:
-        data_frame = current_host.sent_frame(
+        current_frame = current_host.sent_frame(
             channel_is_idle,
-            generate_backoff_time()
+            generate_backoff_time(),
+            timeout * BASE_TIME,
+            DEFAULT_DIFS,
+            DEFAULT_SIFS
         )
-        if data_frame is not None:
-            channel.append(data_frame)
-            current_host.reset(DEFAULT_DIFS, DEFAULT_SIFS)
+        if current_frame is not None:
+            channel.append(current_frame)
 
 # results
 print(conflict_times)
